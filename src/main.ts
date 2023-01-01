6731c90f43132d0a18eb540df067f43bed0e5317
chandra <chandrapenugonda655@gmail.com>
2023-01-01 21:59:29 +0530

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,24 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app/app.module';
+import { FeedModule } from './feed/feed.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder()
+    .setTitle('Fake Linkedin api')
+    .setDescription('The Fake Linkedin API documentation')
+    .setVersion('1.0')
+    .addTag('Linkedin')
+    .build();
+  const document = SwaggerModule.createDocument(app, config, {
+    deepScanRoutes: true,
+    include: [FeedModule],
+  });
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
